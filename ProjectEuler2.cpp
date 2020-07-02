9f7e5393eb0d99584592bf09584400dca613e920
FlavitoAdr <67709192+FlavitoAdr@users.noreply.github.com>
2020-07-02 07:38:03 -0300

    Update ProjectEuler2.cpp

--- ProjectEuler2.cpp
+++ ProjectEuler2.cpp
@@ -20,3 +20,11 @@ int main(){
 	
 	return 0;
 }
+
+/*
+Cada novo termo na sequência de Fibonacci é gerado adicionando os dois termos anteriores. Começando com 1 e 2, os 10 primeiros termos serão:
+
+1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
+
+Considerando os termos da sequência de Fibonacci cujos valores não excedem quatro milhões, encontre a soma dos termos com valores pares.
+*/
