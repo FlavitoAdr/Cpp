6ae22f4017d7ead4433565bc022289c899749670
FlavitoAdr <67709192+FlavitoAdr@users.noreply.github.com>
2020-07-02 07:49:19 -0300

    Update ProjectEuler12.cpp

--- ProjectEuler12.cpp
+++ ProjectEuler12.cpp
@@ -18,3 +18,22 @@ int main(){
 		
 	return 0;
 }
+
+/*
+A sequência dos números dos triângulos é gerada adicionando os números naturais. Portanto, o número do 7º triângulo seria 1 + 2 + 3 + 4 + 5 + 6 + 7 = 28. Os dez primeiros termos seriam:
+
+1, 3, 6, 10, 15, 21, 28, 36, 45, 55, ...
+
+Vamos listar os fatores dos sete primeiros números do triângulo:
+
+ 1 : 1
+ 3 : 1,3
+ 6 : 1,2,3,6
+10 : 1,2,5,10
+15 : 1,3,5,15
+21 : 1,3,7,21
+28 : 1,2, 4,7,14,28
+Podemos ver que 28 é o primeiro número do triângulo a ter mais de cinco divisores.
+
+Qual é o valor do primeiro número do triângulo para ter mais de quinhentos divisores?
+*/
